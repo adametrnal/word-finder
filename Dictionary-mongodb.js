8b9ea82466ede54ad04316b3dbb397d4b55a273f
Adam Brill <brill.adam@gmail.com>
2013-04-04 14:32:04 -0700

    Add MongoLab authentication for DB

--- Dictionary-mongodb.js
+++ Dictionary-mongodb.js
@@ -1,29 +1,41 @@
-//var	mongo = require('mongodb');
 var Db = require('mongodb').Db;
 var Connection = require('mongodb').Connection;
 var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
-var mongoUri = process.env.MONGOLAB_URI || 
+var mongoEnv = process.env.MONGOLAB_URI || 
   process.env.MONGOHQ_URL || 
   'localhost';
 
-var mongoPort = 27017; 
+var mongoHost,
+	mongoPort;
+
+if(mongoEnv !== 'localhost'){
+	mongoHost = 'ds047057.mongolab.com';
+	mongoPort = 47057;
+}
+else {
+	mongoHost = 'localHost';
+	mongoPort = 27017;
+}
+ 
 
 console.log(mongoUri);
 
 // Main DB provider object
 DictionaryProvider = function() {
-    this.db = new Db("dictionary-db", new Server(mongoUri, mongoPort, { auto_reconnect: true }, {}));
+    this.db = new Db('dictionary-db', new Server(mongoHost, mongoPort, { auto_reconnect: true }, {}));
     this.db.open(function(error, db){
-        //db.authenticate(user, pass, function(error, result) {});
+        if(mongoEnv !== 'localhost') {
+        	db.authenticate('heroku_app14477971', '6d42irhc81cd0ep4jb3d3t4hef', function(error, result) {});
+        }
     });
 };
 
 // This gets us in the to 'locations' collection, which is where
 // the farmstand records live.
-DictionaryProvider.prototype.getDictionaries= function(callback) {
+DictionaryProvider.prototype.getDictionaries = function(callback) {
     this.db.collection('dictionaries', function(error, dictionary_collection) {
 		if (error) {
 			callback(error);
