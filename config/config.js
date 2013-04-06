5df20da77cfb1d78e3ae8ac103db38045192a0d8
Adam Brill <brill.adam@gmail.com>
2013-04-06 16:23:55 -0700

    Changing config info for deployment

--- config/config.js
+++ config/config.js
@@ -10,11 +10,11 @@ Config.dbInfo = {
 }
 
 if (Config.mongoEnv !== 'localhost') {
-    Config.mongoHost = 'ABC';
-    Config.mongoPort = 123;
-    Config.dbName = 'ABC';
-    Config.userName = 'ABC';
-    Config.password = 'ABC';
+    Config.mongoHost = 'ds047057.mongolab.com';
+    Config.mongoPort = 47057;
+    Config.dbName = 'heroku_app14477971';
+    Config.userName = 'heroku_app14477971';
+    Config.password = '6d42irhc81cd0ep4jb3d3t4hef';
 } else {
     Config.mongoHost = 'localHost';
     Config.mongoPort = 27017;
